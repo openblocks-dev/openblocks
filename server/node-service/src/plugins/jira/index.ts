d68b6cc3809c3963ebc5c505aa0b2510ae168f11
sarike <sarike@timefly.cn>
2023-02-27 16:50:21 +0800

    feat: some api datasources based on open api

---
+++ server/node-service/src/plugins/jira/index.ts
@@ -0,0 +1,70 @@
+import _ from "lodash";
+import { OpenAPIV3, OpenAPI } from "openapi-types";
+import { ConfigToType, DataSourcePlugin } from "openblocks-sdk/dataSource";
+import { runOpenApi } from "../openApi";
+import { parseOpenApi, ParseOpenApiOptions } from "../openApi/parse";
+
+import spec from "./jira.spec.json";
+
+const dataSourceConfig = {
+  type: "dataSource",
+  params: [
+    {
+      type: "groupTitle",
+      key: "basicAuth",
+      label: "HTTP Basic Auth",
+    },
+    {
+      type: "textInput",
+      key: "basicAuth.username",
+      label: "Username",
+      tooltip: "Basic auth username",
+      placeholder: "<Basic Auth Username>",
+    },
+    {
+      type: "password",
+      key: "basicAuth.password",
+      label: "Password",
+      tooltip: "Basic auth password",
+      placeholder: "<Basic Auth Password>",
+    },
+  ],
+} as const;
+
+const parseOptions: ParseOpenApiOptions = {
+  actionLabel: (method: string, path: string, operation: OpenAPI.Operation) => {
+    return _.upperFirst(operation.operationId || "");
+  },
+};
+
+type DataSourceConfigType = ConfigToType<typeof dataSourceConfig>;
+
+const jiraPlugin: DataSourcePlugin<any, DataSourceConfigType> = {
+  id: "jira",
+  name: "Jira",
+  icon: "jira.svg",
+  category: "api",
+  dataSourceConfig,
+  queryConfig: async () => {
+    const { actions, categories } = await parseOpenApi(spec, parseOptions);
+    return {
+      type: "query",
+      label: "Action",
+      categories: {
+        label: "Category",
+        items: categories,
+      },
+      actions,
+    };
+  },
+  run: function (actionData, dataSourceConfig): Promise<any> {
+    const runApiDsConfig = {
+      url: "",
+      serverURL: "",
+      dynamicParamsConfig: dataSourceConfig,
+    };
+    return runOpenApi(actionData, runApiDsConfig, spec as unknown as OpenAPIV3.Document);
+  },
+};
+
+export default jiraPlugin;
---
+++ server/node-service/src/plugins/sendGrid/index.ts
@@ -0,0 +1,50 @@
+import _ from "lodash";
+import { OpenAPIV3, OpenAPI } from "openapi-types";
+import { ConfigToType, DataSourcePlugin } from "openblocks-sdk/dataSource";
+import { runOpenApi } from "../openApi";
+import { parseOpenApi, ParseOpenApiOptions } from "../openApi/parse";
+
+import spec from "./sendGrid.spec.json";
+
+const dataSourceConfig = {
+  type: "dataSource",
+  params: [],
+} as const;
+
+const parseOptions: ParseOpenApiOptions = {
+  actionLabel: (method: string, path: string, operation: OpenAPI.Operation) => {
+    return _.upperFirst(operation.operationId || "");
+  },
+};
+
+type DataSourceConfigType = ConfigToType<typeof dataSourceConfig>;
+
+const sendGridPlugin: DataSourcePlugin<any, DataSourceConfigType> = {
+  id: "sendGrid",
+  name: "SendGrid",
+  icon: "sendGrid.svg",
+  category: "api",
+  dataSourceConfig,
+  queryConfig: async () => {
+    const { actions, categories } = await parseOpenApi(spec, parseOptions);
+    return {
+      type: "query",
+      label: "Action",
+      categories: {
+        label: "Category",
+        items: categories,
+      },
+      actions,
+    };
+  },
+  run: function (actionData, dataSourceConfig): Promise<any> {
+    const runApiDsConfig = {
+      url: "",
+      serverURL: "",
+      dynamicParamsConfig: dataSourceConfig,
+    };
+    return runOpenApi(actionData, runApiDsConfig, spec as unknown as OpenAPIV3.Document);
+  },
+};
+
+export default sendGridPlugin;
