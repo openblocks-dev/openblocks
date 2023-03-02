0765d7b4dc2061f7f60be68c7fdf11275b070850
Ming Fang <mingfang@mac.com>
2023-03-02 03:29:01 +0000

    add Mermaid component

--- client/packages/openblocks-comps/src/index.ts
+++ client/packages/openblocks-comps/src/index.ts
@@ -1,7 +1,9 @@
 import { ChartCompWithDefault } from "./comps/chartComp/chartComp";
 import { ImageEditorComp } from "./comps/imageEditorComp/index";
+import {MermaidComp} from "./comps/mermaidComp";
 
 export default {
   chart: ChartCompWithDefault,
   imageEditor: ImageEditorComp,
+  mermaid: MermaidComp,
 };
