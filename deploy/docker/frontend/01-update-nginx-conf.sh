8180e38a5d8cf3f529ca743e1ef7da7dceabe9e6
David Senk <david@senk.me>
2023-03-30 16:21:01 -0400

    frontend body size env var for nginx

--- deploy/docker/frontend/01-update-nginx-conf.sh
+++ deploy/docker/frontend/01-update-nginx-conf.sh
@@ -4,7 +4,10 @@ set -e
 
 sed -i "s@__OPENBLOCKS_API_SERVICE_URL__@${OPENBLOCKS_API_SERVICE_URL:=http://localhost:8080}@" /etc/nginx/nginx.conf
 sed -i "s@__OPENBLOCKS_NODE_SERVICE_URL__@${OPENBLOCKS_NODE_SERVICE_URL:=http://localhost:6060}@" /etc/nginx/nginx.conf
+sed -i "s@__OPENBLOCKS_FRONTEND_BODY_SIZE__@${OPENBLOCKS_FRONTEND_BODY_SIZE:=1M}@" /etc/nginx/nginx.conf
+
 
 echo "nginx config updated with:"
 echo "    Openblocks api service URL: ${OPENBLOCKS_API_SERVICE_URL}"
 echo "   Openblocks node service URL: ${OPENBLOCKS_NODE_SERVICE_URL}"
+echo " Openblocks Frontend Body Size: ${OPENBLOCKS_FRONTEND_BODY_SIZE}"
